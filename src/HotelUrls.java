14397078e42332c896aca3392d4fee1bbbd3aaa2
AbdullahRahahleah <man.real300@yahoo.com>
2017-06-15 20:31:08 +0300

    Add files via upload

---
+++ src/HotelUrls.java
@@ -0,0 +1,19 @@
+
+public class HotelUrls {
+	public String getHotelInfositeUrl() {
+		return hotelInfositeUrl;
+	}
+	public void setHotelInfositeUrl(String hotelInfositeUrl) {
+		this.hotelInfositeUrl = hotelInfositeUrl;
+	}
+	public String getHotelSearchResultUrl() {
+		return hotelSearchResultUrl;
+	}
+	public void setHotelSearchResultUrl(String hotelSearchResultUrl) {
+		this.hotelSearchResultUrl = hotelSearchResultUrl;
+	}
+	private String hotelInfositeUrl;
+	private String hotelSearchResultUrl;
+	
+
+}
---
+++ src/Offers.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Offers {
+	private ArrayList<hotel> Hotel;
+	public ArrayList<hotel> getHotel() {
+		return Hotel;
+	}
+
+	public void setHotel(ArrayList<hotel> hotel) {
+		Hotel = hotel;
+	}
+
+
+	public Offers() {
+	}
+}
